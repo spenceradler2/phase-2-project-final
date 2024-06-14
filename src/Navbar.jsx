60e027bcedef18efb9dbfa2e17a573d548ebfb62
Spencer <spencer.adler2@gmail.com>
2024-06-14 18:46:18 -0400

    added navbar component

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li><Link to="/Header">Home</Link></li>
+      <li><Link to="/TravelPlanList">See Everyone's Travel Plans</Link></li>
+      <li><Link to="/NewTravelForm">Add a Travel Plan</Link></li>
+    </ul>
+  )
+}
+
+export default Navbar
